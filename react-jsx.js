ddf4159f009b21d919fe518c5a6adb150d1c13ee
Jackson Ray Hamilton <jackson@jacksonrayhamilton.com>
2019-12-03 12:25:05 -0800

    Add rules for React Hooks

--- react-jsx.js
+++ react-jsx.js
@@ -7,13 +7,16 @@ module.exports = {
     }
   },
   plugins: [
-    'react'
+    'react',
+    'react-hooks'
   ],
   rules: {
     'react/react-in-jsx-scope': ['warn'],
     'react/jsx-no-duplicate-props': ['warn'],
     'react/jsx-no-undef': ['warn'],
     'react/jsx-uses-react': ['warn'],
-    'react/jsx-uses-vars': ['warn']
+    'react/jsx-uses-vars': ['warn'],
+    'react-hooks/rules-of-hooks': ['warn'],
+    'react-hooks/exhaustive-deps': ['warn']
   }
 }
