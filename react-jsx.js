9743a7ee5e1043aa9525f0137e1cc9824c289de8
Jackson Ray Hamilton <jackson@jacksonrayhamilton.com>
2021-02-12 21:35:57 -0800

    Look for defined variables in the global scope, too

--- react-jsx.js
+++ react-jsx.js
@@ -13,7 +13,7 @@ module.exports = {
   rules: {
     'react/react-in-jsx-scope': ['warn'],
     'react/jsx-no-duplicate-props': ['warn'],
-    'react/jsx-no-undef': ['warn'],
+    'react/jsx-no-undef': ['warn', {'allowGlobals': true}],
     'react/jsx-uses-react': ['warn'],
     'react/jsx-uses-vars': ['warn'],
     'react-hooks/rules-of-hooks': ['warn'],
